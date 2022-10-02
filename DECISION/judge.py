44f22168150708521d0d895d8e6992a05e606d12
di <di.github.com>
2022-10-02 17:59:18 +0900

    判定問題: 判定

---
+++ DECISION/judge.py
@@ -0,0 +1,22 @@
+from sys import stdin
+
+# N: ビット数
+# S: 総和
+# AB: 各ビットが取る値
+N, S = [int(x) for x in stdin.readline().strip().split()]
+AB = [[int(x) for x in stdin.readline().strip().split()] for _ in range(N)]
+
+# 先頭からiビットの合計がS以下の値となりうるか
+dp = [[0] * (S + 1) for _ in range(N + 1)]
+dp[0][0] = 1
+for i in range(N):
+    a, b = AB[i]
+    for j in range(S + 1):
+        if dp[i][j]:
+            if j + a <= S:
+                dp[i + 1][j + a] = 1
+            if j + b <= S:
+                dp[i + 1][j + b] = 1
+
+print(f"総和がSになる: {dp[N][S]}")
+[print(x) for x in dp]
